7563b6dad0bbabd0c24f89a4c319f36a9351f605
janb <philipjanbaruis@gmail.com>
2022-11-21 14:22:25 +0800

    update dependencies

--- app/src/main/java/com/example/Compose2022/MainActivity.kt
+++ app/src/main/java/com/example/Compose2022/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.Compose2022
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.Compose2022.ui.theme.Compose2022Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                   MessageCard(msg = Message(
+                       name = "Philip Jan Baruis",
+                       address = "Davao City"
+                   ))
                 }
             }
         }
@@ -40,4 +48,45 @@ fun DefaultPreview() {
     Compose2022Theme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun textViewCompose(name: String) {
+    Text("Hello $name!")
+}
+
+@Preview
+@Composable
+fun previewTextViewCompose() {
+    textViewCompose(name = "Philip Jan!")
+}
+
+data class Message(val name: String, val address: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier
+        .padding(3.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "launcher bg",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier
+            .padding(4.dp))
+
+        Column() {
+            Text(text = "Name: ${msg.name}")
+            Text(text = "Address: ${msg.address}")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MessageCardPreview() {
+    MessageCard(msg = Message("PhilipJ", "Dvo 7"))
 }
